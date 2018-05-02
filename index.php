ea76ccd6a91f933a860cabbc820f7a14b19ef369
pitufina3 <pitufina3@.com>
2018-05-02 12:15:41 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,186 @@
+<!doctype html>
+
+<html lang="en">
+
+  <head>
+
+    <!-- Required meta tags -->
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+
+    <!-- Bootstrap CSS -->
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+
+
+    <title>Hello, world!</title>
+
+  </head>
+
+  <body>
+    
+    <header>
+    
+    </header>
+
+<nav class="navbar navbar-expand-lg">
+    <div>
+        <img src="imagenes/imagen.jpg" alt="" height="80%" witdh="560%">
+    </div>
+
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+
+        <div class="navbar-nav">
+
+            <a class="nav-item nav-link" href="#">Furniture</a>
+
+            <a class="nav-item nav-link" href="#">Workspaces</a>
+
+            <a class="nav-item nav-link " href="#">Open Making</a>
+
+            <a class="nav-item nav-link " href="#">Blog</a>
+           
+        </div>
+
+    </div>
+    
+
+</nav>
+  
+    <div class="titulo"> 
+    <img src="imagenes/imagen1.jpg" witdh="360%">
+
+    </div>
+
+    <div class="card-group">
+        <div class="card">
+            <img class="card-img-top" src="imagenes/mesa.jpg" alt="Lean Desk">
+                <div class="card-body">
+                    <h5 class="card-title">Lean Desk</h5>
+                    <p class="card-text"><small class="text-muted"><?=("Generous four seat workstation");?></small></p>
+                    <br>
+                    <br>
+                </div>
+            </div>
+        <div class="card">
+            <img class="card-img-top" src="imagenes/estanteria.jpg" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">Linnea Bookshelf </h5>
+                    <p class="card-text"><small class="text-muted"><?=("Elegant shelving & storage solution");?></small></p>
+                </div>
+            </div>
+        <div class="card">
+            <img class="card-img-top" src="imagenes/mesapequeña.jpg" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">Johann Bench</h5>
+                    <p class="card-text"><small class="text-muted"><?=("2 person canteen and meeting bench");?></small></p>
+                </div>
+        </div>  
+    </div>
+
+    <div class=>
+
+    
+            <img class="" src="imagenes/hombre.jpg" alt="100%" witdh="360%">
+    </div>
+
+        <h2>HOW IT WORKS
+        </h2> 
+
+    <div class="card-group">
+        <div class="card">
+            <img class="card-img-top" src="" alt="">
+                <div class="card-body">
+                <i class="fas fa-table"></i>
+                    <h5 class="card-title">Select desing</h5>
+                    <p class="card-text"><small class="text-muted"><?=("Browse and add designs to your basket or get in touch for help");?></small></p>
+                </div>
+            </div>
+        <div class="card">
+            <img class="card-img-top" src="" alt="">
+                <div class="card-body">
+                <i class="fas fa-euro-sign"></i>
+                    <h5 class="card-title">Request quotes </h5>
+                    <p class="card-text"><small class="text-muted"><?=("Get quotes from local makers in your area within two business days");?></small></p>
+                </div>
+            </div>
+        <div class="card">
+            <img class="card-img-top" src="" alt="">
+                <div class="card-body">
+                <i class="fas fa-heart"></i>
+                    <h5 class="card-title">Choose your maker</h5>
+                    <p class="card-text"><small class="text-muted"><?=("Choose the local maker’s quote that works best for you");?></small></p>
+                </div>
+        </div>  
+
+        <div class="card">
+            <img class="card-img-top" src="" alt="">
+                <div class="card-body">
+                <i class="fas fa-dove"></i>
+                    <h5 class="card-title">Receive your furniture</h5>
+                    <p class="card-text"><small class="text-muted"><?=("Get furniture delivered to your door in 2-4 weeks, direct from your maker");?></small></p>
+                </div>
+        </div>  
+    </div>
+ 
+    <span class="button-container text-left">
+  
+        <a href="/designs" data-track-link="true" data-track-cta="View" data-track-type="how_it_works" data-track-id="21333" class="btn btn-lg btn-primary btn-min-width ">
+    Get started</a>
+    </span>
+
+    <div class ="img container">
+        <img src="imagenes/nike.jpg" alt="" witdh="200%">
+        <img src="imagenes/wework.jpg" alt="" witdh="200%">
+        <img src="imagenes/google.jpg" alt="" witdh="200%">
+        <img src="imagenes/greenpeace.jpg" alt="" witdh="200%">
+    </div>
+
+    <p3>The Guardian</p3>
+    <br>
+    <br>
+    <p4> “Opendesk is changing the way furniture is produced and moved, cutting
+         out time-consuming and expensive shipping, showrooms and storage.”</p4>
+    
+    <div>
+        <img src="imagenes/fastcompany.jpg" alt="" witdh="200%">
+        <img src="imagenes/wired.jpg" alt="" witdh="200%">
+        <img src="imagenes/techcrunch.jpg" alt="" witdh="200%">
+        <img src="imagenes/theguardian.jpg" alt="" witdh="200%">
+        <img src="imagenes/dezeen.jpg" alt="" witdh="200%">
+    </div>
+
+
+    <footer>
+        
+
+
+
+
+    </footer>
+
+            
+
+
+    <!-- Optional JavaScript -->
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="app.js"></script>
+  </body>
+
+</html>
+
---
+++ indexjesus.php
@@ -0,0 +1,180 @@
+<!doctype html>
+
+<html lang="en">
+
+  <head>
+
+    <!-- Required meta tags -->
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+
+    <!-- Bootstrap CSS -->
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="stylejesus.css">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+
+
+    <title>Hello, world!</title>
+
+  </head>
+
+  <body>
+    
+  <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a href=""><img src="imagenes/imagen.jpg" height="80" alt="OpenDesk"></a>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Furniture <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Workspaces</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Open making</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Blog</a>
+                    </li>
+                </ul>
+                <span class="navbar-text">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#"> <i class="fas fa-shopping-cart"></i> <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Your dashboard</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Join as a maker</a>
+                        </li>
+                    </ul>
+                </span>
+            </div>
+        </nav>
+
+            
+    </header>
+<div id="uno" class="filaimagen">
+    <div>
+            <h2> Furniture designed for<br> inspirng workplaces
+            <p></p>
+            <h4> We're changing the way furniture is<br> made,
+            by connecting customers to local<br> makers</h4>
+            <p></p>
+            <button class="btn btn-sucess">Shop Furniture</button>
+       
+    </div>
+
+
+</div>
+<div id="muebles">
+    <div class="card-group">
+    
+        <div class="card">
+            <img class="card-img-top" src="imagenes/mesa.jpg" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">Lean</h5>
+            <p class="card-text">Generous four seat workstation</p>
+            <p class="card-text"><small class="text-muted"></small></p>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="imagenes/estanteria.jpg" alt="">
+            <div class="card-body">
+            <h5 class="card-title">Linnea  Bookshelf</h5>
+            <p class="card-text">Elegant shelving & storage solution</p>
+            <p class="card-text"><small class="text-muted"></small></p>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="imagenes/mesapequeña.jpg" alt="">
+            <div class="card-body">
+            <h5 class="card-title">Johann Bench</h5>
+            <p class="card-text"></p>2 person canteen and meeting bench
+            <p class="card-text"><small class="text-muted"></small></p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div id="dos" class="filaimagen">
+    <div>
+            <h2> A global platform for local<br>making</h2>
+            <p></p>
+            <h4> Opendesk is an online marketplace<br>
+                 that hosts independently designed<br>
+                  furniture and connects its customers<br>
+                   to local makers around the world.<br>
+                    Rather than mass manufacturing and shipping<br>
+                     worldwide, we’re building a distributed and<br>
+                      ethical supply chain<br>
+                       through a global maker network.<br>
+            by connecting customers to local<br> makers</h4>
+            <p></p>
+       
+    </div> 
+</div> 
+<div id="pasos" class="text-center">
+    <p>HOW IT WORKS</P>
+<div class="row text-center">
+    <div class="col">
+        <h1><i class="fab fa-angular"></i></h1>
+        <h3>Select design</h3>
+        <p>Extraordinarios diseños realizados por Hermeregildo</p>
+    </div>
+
+    <div class="col">
+    <h1><i class="fab fa-node-js"></i></h1>
+    <h3>Request quotes</h3>
+        <p>Consigue presupuestos personalizados</p>
+    </div>
+
+    <div class="col">
+    <h1><i class="fab fa-laravel"></i></h1>
+    <h3>Choose your maker</h3>
+        <p>Elige entre cientos y miles de diseños</p>
+    </div>
+
+    <div class="col">
+    <h1><i class="fab fa-react"></i></h1>
+    <h3>Receive your furniture</h3>
+        <p>Recibe cómodamente tus muebles en casa</p>
+    </div>
+    
+
+
+</div>
+
+<button class="btn btn-sucess">Get started</button>
+
+<div id="mivideo">
+<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/215745438" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+<p><a href="https://vimeo.com/215745438"</a>
+</div>
+
+
+
+    <!-- Optional JavaScript -->
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="app.js"></script>
+  </body>
+
+</html>
+
